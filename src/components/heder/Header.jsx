0d276af6a091d4680f0e17d1501cef9499648500
Emilya <emilyaxachatryan.ait@gmail.com>
2024-12-07 13:48:52 +0400

    I added new information to the file, and created a new departments.jsx file in the departments folder.

--- src/components/heder/Header.jsx
+++ src/components/heder/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../../App.css'
+import logo from  '../../../public/photos/logo.svg'
 const Header = () => {
   return (
-    <div className='text-3xl font-bold'>Hello</div>
+    <div className='header'>
+        <div className='w-24 h-10 ' >
+        <img src={logo}alt="list.amlogo" />
+      
+        </div>
+    </div>
   )
 }
 
